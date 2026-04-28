5226f2da935f788c0162393e96b96f357c068d1c
Shahil Akhatar <shahilakhatar83@gmail.com>
2026-04-28 23:22:14 +0530

    implementing get/post routes

--- index.js
+++ index.js
@@ -8,10 +8,36 @@ app.use(express.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res)=>{
-    res.send("Serving Working Well NASA ...");
+ let posts = [
+    {
+        username:"Shahil",
+        content:"I love codding!",
+    },
+     {
+        username:"Faisal",
+        content:"I got selected for my 1st internship!",
+    },
+     {
+        username:"Rasid darling",
+        content:"Servig working well!",
+    },
+ ]
+
+app.get("/posts", (req, res)=>{
+    res.render("index.ejs",{posts});
+});
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+   let { username,content}=req.body;
+   posts.push({username,content});
+   res.send("post request") 
+    res.send("Post request working...");
 });
 
 app.listen(port,()=>{
